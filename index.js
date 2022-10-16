cb7ae88abf1c8c139220cfea37aa459ddf128b27
Dmitry Stashevsky <dmitrystash@mail.ru>
2022-10-16 23:41:22 +0300

    Fixes

--- index.js
+++ index.js
@@ -7,13 +7,13 @@ const token = '5624025318:AAEy-wbMfuPT5iF4hO2niyoHGzTo3FA_teA';
 const bot = new TelegramBot(token, {polling: true});
 
 // Matches "/echo [whatever]"
-bot.onText(/\/echo (.+)/, (msg, match) => {
+bot.onText(/Занятия/, (msg, match) => {
   // 'msg' is the received Message from Telegram
   // 'match' is the result of executing the regexp above on the text content
   // of the message
 
   const chatId = msg.chat.id;
-  const resp = match[1]; // the captured "whatever"
+  const resp = "Понедельник/Среда/Пятница 19 00"
 
   // send back the matched "whatever" to the chat
   bot.sendMessage(chatId, resp);
